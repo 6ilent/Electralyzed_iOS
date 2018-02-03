0bb6921fab9417321a42a1a8951b2d332dc0f45a
6ilent <pkmnx7802@gmail.com>
2018-02-02 18:33:23 -0700

    Basic UI Done! NO ACTUAL CODE YET!

---
+++ iElectralyzed/iElectralyzed/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  iElectralyzed
+//
+//  Created by Jose Velasquez on 2/2/18.
+//  Copyright © 2018 6ilent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
